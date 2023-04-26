f18cdd0aa4b25d5fbd81741ca9ab3e7006d79342
GaetanSantucci <gaetan.santucci@outlook.com>
2023-04-26 23:12:01 +0200

    :boom: page marketing OK, start Project page

--- src/Components/Marketing/index.js
+++ src/Components/Marketing/index.js
@@ -1,4 +1,4 @@
-import { BackgroundMarketing, Dot } from '../SvgComponent';
+import { BackgroundMarketing, Dot, IconPerformance, IconSeo, IconUX } from '../SvgComponent';
 import './services.scss';
 
 export const MarketingPage = () => {
@@ -14,7 +14,7 @@ export const MarketingPage = () => {
           <div className='service-container-main-benefit-card'>
             <h3 className='service-container-main-benefit-card-title'>Référencement</h3>
             <div className='service-container-main-benefit-card-content'>
-              <div className='service-container-main-benefit-card-content-icon'>Icon</div>
+              <div className='service-container-main-benefit-card-content-icon'><IconSeo /></div>
               <div className='service-container-main-benefit-card-content-figure'>75%</div>
               <p>Des internautes ne vont pas plus loin que la 1ère page de recherche Google</p>
               <p>C&apos;est pourquoi il est important d&apos;adapter une bonne stratégie  afin d&apos;optimiser son référencement naturel.</p>
@@ -24,20 +24,20 @@ export const MarketingPage = () => {
           <div className='service-container-main-benefit-card'>
             <h3 className='service-container-main-benefit-card-title'>Performances</h3>
             <div className='service-container-main-benefit-card-content'>
-              <div className='service-container-main-benefit-card-content-icon'>Icon</div>
+              <div className='service-container-main-benefit-card-content-icon'><IconPerformance /></div>
               <div className='service-container-main-benefit-card-content-figure'>83%</div>
               <p>Des consommateurs pensent qu&apos;une expèrience fluide est importante pour leur décision d&apos;achat</p>
               <p>Notre savoir des clients nous permets de comprendre leurs attentes, nous développons des sites en prenant en compte vos attentes. </p>
-              <p>Un site fluide et performant permets de fidéliser sa clientèle et d&apos;augmenter son chiffre d&apos;affaire</p>
+              <p>Un site fluide et performant permets de fidéliser sa clientèle et d&apos;augmenter son chiffre d&apos;affaire.</p>
             </div>
           </div>
           <div className='service-container-main-benefit-card'>
             <h3 className='service-container-main-benefit-card-title'>Expérience utilisateur</h3>
             <div className='service-container-main-benefit-card-content'>
-              <div className='service-container-main-benefit-card-content-icon'>Icon</div>
+              <div className='service-container-main-benefit-card-content-icon'><IconUX /></div>
               <div className='service-container-main-benefit-card-content-figure'>74%</div>
               <p>Des visiteurs sont succeptible de revenir sur un site s&apos;il est optimiser pour leur mobile</p>
-              <p>Ici, nous pensons mobile dès l&apos;élaboration de votre projet car 64% des recherches se font désormais sur mobile et qu&apos;il est impératif d&apos;avoir un site adaptatif</p>
+              <p>Ici, nous pensons mobile dès l&apos;élaboration de votre projet car 64% des recherches se font désormais sur mobile et qu&apos;il est impératif d&apos;avoir un site adaptatif.</p>
             </div>
           </div>
         </div>
@@ -48,9 +48,9 @@ export const MarketingPage = () => {
       <div className='service-container-background-dot'>
         <Dot />
       </div>
-      <div className='sticky-container'>
-        <p>02</p>
-      </div>
-    </section>
+      {/* <div className='sticky-container'>
+        <p>02</p> */}
+      {/* </div> */}
+    </section >
   )
 }
